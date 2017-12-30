a70bcf931937da248c3bc0db629bafb448ea967c
Sergey Sova <i.am@lestad.net>
2017-12-30 17:42:59 +0300

    Add flow

--- flow-typed/npm/cheerio_v1.0.x.js
+++ flow-typed/npm/cheerio_v1.0.x.js
@@ -13,89 +13,189 @@
  * https://github.com/flowtype/flow-typed
  */
 
+type $Spread<A, B> = /* ::{
+  ...$Exact<A>,
+  ...$Exact<B>,
+}
+type noop1<A, B> = */ A & B
+
 declare module 'cheerio' {
-  declare module.exports: any;
-}
+  declare type Cheerio = {|
+    [index: number]: CheerioElement,
+    length: number,
 
-/**
- * We include stubs for each file inside this npm package in case you need to
- * require those files directly. Feel free to delete any files that aren't
- * needed.
- */
-declare module 'cheerio/lib/api/attributes' {
-  declare module.exports: any;
-}
+    attr: (name: string) => string,
 
-declare module 'cheerio/lib/api/css' {
-  declare module.exports: any;
-}
+    data(name?: string, value?: any): any,
 
-declare module 'cheerio/lib/api/forms' {
-  declare module.exports: any;
-}
+    val(value?: string): Cheerio,
+    removeAttr(name: string): Cheerio,
+    has(selector: string | CheerioElement): Cheerio,
 
-declare module 'cheerio/lib/api/manipulation' {
-  declare module.exports: any;
-}
+    hasClass(className: string): boolean,
+    addClass(classNames: string): Cheerio,
 
-declare module 'cheerio/lib/api/traversing' {
-  declare module.exports: any;
-}
+    removeClass(className?: string | (index: number, className: string) => string): Cheerio,
 
-declare module 'cheerio/lib/cheerio' {
-  declare module.exports: any;
-}
+    toggleClass(className: string, toggleSwitch?: boolean): Cheerio,
+    toggleClass(toggleSwitch?: boolean): Cheerio,
+    toggleClass(func: (index: number, className: string, toggleSwitch: boolean) => string, toggleSwitch?: boolean): Cheerio,
 
-declare module 'cheerio/lib/options' {
-  declare module.exports: any;
-}
+    is(selector: string | CheerioElement | CheerioElement[] | Cheerio | (index: number, element: CheerioElement) => boolean): boolean,
 
-declare module 'cheerio/lib/parse' {
-  declare module.exports: any;
-}
+    // Form
+    serialize(): string,
+    serializeArray(): { name: string, value: string }[],
 
-declare module 'cheerio/lib/static' {
-  declare module.exports: any;
-}
+    // Traversing
 
-declare module 'cheerio/lib/utils' {
-  declare module.exports: any;
-}
+    find(selector: string | Cheerio): Cheerio,
 
-// Filename aliases
-declare module 'cheerio/index' {
-  declare module.exports: $Exports<'cheerio'>;
-}
-declare module 'cheerio/index.js' {
-  declare module.exports: $Exports<'cheerio'>;
-}
-declare module 'cheerio/lib/api/attributes.js' {
-  declare module.exports: $Exports<'cheerio/lib/api/attributes'>;
-}
-declare module 'cheerio/lib/api/css.js' {
-  declare module.exports: $Exports<'cheerio/lib/api/css'>;
-}
-declare module 'cheerio/lib/api/forms.js' {
-  declare module.exports: $Exports<'cheerio/lib/api/forms'>;
-}
-declare module 'cheerio/lib/api/manipulation.js' {
-  declare module.exports: $Exports<'cheerio/lib/api/manipulation'>;
-}
-declare module 'cheerio/lib/api/traversing.js' {
-  declare module.exports: $Exports<'cheerio/lib/api/traversing'>;
-}
-declare module 'cheerio/lib/cheerio.js' {
-  declare module.exports: $Exports<'cheerio/lib/cheerio'>;
-}
-declare module 'cheerio/lib/options.js' {
-  declare module.exports: $Exports<'cheerio/lib/options'>;
-}
-declare module 'cheerio/lib/parse.js' {
-  declare module.exports: $Exports<'cheerio/lib/parse'>;
-}
-declare module 'cheerio/lib/static.js' {
-  declare module.exports: $Exports<'cheerio/lib/static'>;
-}
-declare module 'cheerio/lib/utils.js' {
-  declare module.exports: $Exports<'cheerio/lib/utils'>;
+    parent(selector?: string): Cheerio,
+    parents(selector?: string): Cheerio,
+    parentsUntil(selector?: string | CheerioElement | Cheerio, filter?: string): Cheerio,
+
+    prop(name: string): any,
+    prop(name: string, value: any): Cheerio,
+
+    closest(): Cheerio,
+    closest(selector: string): Cheerio,
+
+    next(selector?: string): Cheerio,
+    nextAll(selector?: string): Cheerio,
+
+    nextUntil(selector?: string | CheerioElement | Cheerio, filter?: string): Cheerio,
+
+    prev(selector?: string): Cheerio,
+    prevAll(selector?: string): Cheerio,
+    prevUntil(selector?: string | CheerioElement | Cheerio, filter?: string): Cheerio,
+    slice(start: number, end?: number): Cheerio,
+    siblings(selector?: string): Cheerio,
+
+    children(selector?: string): Cheerio,
+    contents(): Cheerio,
+
+    each(func: (index: number, element: CheerioElement) => any): Cheerio,
+    map(func: (index: number, element: CheerioElement) => any): Cheerio,
+
+    filter(selector: string | Cheerio | CheerioElement | CheerioElement[]): Cheerio,
+    filter(func: (index: number, element: CheerioElement) => boolean): Cheerio,
+
+    not(selector: string | Cheerio | CheerioElement): Cheerio,
+    not(func: (index: number, element: CheerioElement) => boolean): Cheerio,
+
+    first(): Cheerio,
+    last(): Cheerio,
+
+    eq(index: number): Cheerio,
+    get(index?: number): string[] | CheerioElement | CheerioElement[],
+    index(selector?: string | Cheerio): number,
+    end(): Cheerio,
+    add(selectorOrHtml: string | CheerioElement | CheerioElement[] | Cheerio, context?: Document): Cheerio,
+    addBack(filter?: string): Cheerio,
+
+    // Manipulation
+    appendTo(target: Cheerio): Cheerio,
+    prependTo(target: Cheerio): Cheerio,
+    append(content: string | Document | Document [] | Cheerio, ...contents: any[]): Cheerio,
+    prepend(content: string | Document | Document[] | Cheerio, ...contents: any[]): Cheerio,
+    after(content: string | Document | Document[] | Cheerio, ...contents: any[]): Cheerio,
+
+    insertAfter(content: string | Document | Cheerio): Cheerio,
+    before(content: string | Document | Document[] | Cheerio, ...contents: any[]): Cheerio,
+    insertBefore(content: string | Document | Cheerio): Cheerio,
+    remove(selector?: string): Cheerio,
+    replaceWith(content: string | CheerioElement | CheerioElement[] | Cheerio | () => Cheerio): Cheerio,
+
+    empty(): Cheerio,
+
+
+    html: () => string | (html: string) => Cheerio,
+    text: () => string | (text: string) => Cheerio,
+
+    // See https://github.com/cheeriojs/cheerio/issues/731
+    /*wrap(content: string): Cheerio,
+    wrap(content: Document): Cheerio,
+    wrap(content: Cheerio): Cheerio,*/
+
+    css(propertyName: string): string,
+    css(propertyNames: string[]): string[],
+    css(propertyName: string, value: string): Cheerio,
+    css(propertyName: string, value: number): Cheerio,
+    css(propertyName: string, func: (index: number, value: string) => string): Cheerio,
+    css(propertyName: string, func: (index: number, value: string) => number): Cheerio,
+    css(properties: Object): Cheerio,
+
+    // Rendering
+
+    // Miscellaneous
+
+    clone(): Cheerio,
+
+    // Not Documented
+
+    toArray(): CheerioElement[],
+  |}
+
+  declare type CheerioElement = {|
+    // Document References
+    // Node Console
+    tagName: string,
+    type: string,
+    name: string,
+    attribs: { [attr: string]: string },
+    children: CheerioElement[],
+    childNodes: CheerioElement[],
+    lastChild: CheerioElement,
+    firstChild: CheerioElement,
+    next: CheerioElement,
+    nextSibling: CheerioElement,
+    prev: CheerioElement,
+    previousSibling: CheerioElement,
+    parent: CheerioElement,
+    parentNode: CheerioElement,
+    nodeValue: string,
+    data?: string,
+  |}
+
+  declare type CheerioOptionsInterface = {|
+    // Document References
+    // Cheerio https://github.com/cheeriojs/cheerio
+    // HTMLParser2 https://github.com/fb55/htmlparser2/wiki/Parser-options
+    // DomHandler https://github.com/fb55/DomHandler
+
+    xmlMode?: boolean,
+    decodeEntities?: boolean,
+    lowerCaseTags?: boolean,
+    lowerCaseAttributeNames?: boolean,
+    recognizeCDATA?: boolean,
+    recognizeSelfClosing?: boolean,
+    normalizeWhitespace?: boolean,
+  |}
+
+  declare type CheerioSelector =
+    | (selector: string | Cheerio | CheerioElement) => Cheerio
+    | (selector: string | Cheerio | CheerioElement, context: string | CheerioElement | CheerioElement[] | Cheerio, root: string) => Cheerio
+
+
+  declare type CheerioStatic = {|
+    (selector: string | Cheerio | CheerioElement): Cheerio,
+    // Document References
+    // Cheerio https://github.com/cheeriojs/cheerio
+    // JQuery http://api.jquery.com
+    xml(): string,
+    root(): Cheerio,
+    contains(container: CheerioElement, contained: CheerioElement): boolean,
+    parseHTML(data: string, context?: Document, keepScripts?: boolean): Document[],
+
+    html(options?: CheerioOptionsInterface): string,
+    html(selector: string | Cheerio | CheerioElement, options?: CheerioOptionsInterface): string,
+  |}
+
+  declare type CheerioAPI = $Spread<CheerioStatic, {|
+    (selector: string | Cheerio | CheerioElement): Cheerio,
+    load(html: string | CheerioElement, options?: CheerioOptionsInterface): CheerioStatic,
+  |}>
+
+  declare module.exports: CheerioAPI;
 }
