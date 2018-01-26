70b871d103fd24830142cc5c308baa8a72312856
Sergey Sova <mail@sergeysova.com>
2018-01-27 01:39:15 +0300

    build: run rustfmt after build (if present)

--- .cz-config.js
+++ .cz-config.js
@@ -6,6 +6,7 @@ module.exports = {
     { value: 'style', name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)' },
     { value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature' },
     { value: 'perf', name: 'perf:     A code change that improves performance' },
+    { value: 'build', name: 'build:    Changes that affect the build system or external dependencies' },
     { value: 'test', name: 'test:     Adding missing tests' },
     { value: 'chore', name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation' },
     { value: 'revert', name: 'revert:   Revert to a commit' },
