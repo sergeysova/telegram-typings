sergeysova/telegram-typings

commit f42d8730c7015d45b68bd9871c5850ead7ef90be

chore: Apply review comments

lib/parser.js
@@ -106,7 +106,7 @@ async function handleInterface({ name, description, rows }) {
     const fieldName = flName.text().trim()
     let fieldType = flType.text().trim()
 
-    if (fieldName === 'Field' || fieldName === 'Parameters') {
+    if (['Field', 'Parameters'].includes(fieldName)) {
       continue // eslint-disable-line no-continue
     }
 
@@ -114,12 +114,7 @@ async function handleInterface({ name, description, rows }) {
       fieldType = fieldType.split(' or ')
     }
 
-    if (Array.isArray(fieldType)) {
-      debug(`${name.text().trim()}.${fieldName}: ${fieldType.join(' | ')} ::: ${fieldDescription}`)
-    }
-    else {
-      debug(`${name.text().trim()}.${fieldName}: ${fieldType} ::: ${fieldDescription}`)
-    }
+    debug(`${name.text().trim()}.${fieldName}: ${Array.isArray(fieldType) ? fieldType.join(' | ') : fieldType} ::: ${fieldDescription}`)
 
     const field = new Field(
       fieldName,
@@ -159,7 +154,7 @@ async function handleMethod({ name, description, rows }) {
     const fieldName = flName.text().trim()
     let fieldType = flType.text().trim()
 
-    if (fieldName === 'Field' || fieldName === 'Parameters') {
+    if (['Field', 'Parameters'].includes(fieldName)) {
       continue // eslint-disable-line no-continue
     }
 
@@ -167,12 +162,7 @@ async function handleMethod({ name, description, rows }) {
       fieldType = fieldType.split(' or ')
     }
 
-    if (Array.isArray(fieldType)) {
-      debug(`${name.text().trim()}.${fieldName}: ${fieldType.join(' | ')} ::: ${fieldDescription}`)
-    }
-    else {
-      debug(`${name.text().trim()}.${fieldName}: ${fieldType} ::: ${fieldDescription}`)
-    }
+    debug(`${name.text().trim()}.${fieldName}: ${Array.isArray(fieldType) ? fieldType.join(' | ') : fieldType} ::: ${fieldDescription}`)
 
     const field = new Field(
       fieldName,
@@ -215,7 +205,7 @@ async function requestAndParse(store/*: Store*/) {
 
     let typeClass
 
-    if (type !== 'Field' && type !== 'Parameters') {
+    if (['Field', 'Parameters'].includes(type)) {
       continue // eslint-disable-line no-continue
     }
 

commit ef317f11c3500ebe8eb0163351becec1992ba1ac

fix: Bug in parser, breaking type generation
I accidentally caused a bug in f42d8730c7015d45b68bd9871c5850ead7ef90be
when doing https://github.com/sergeysova/telegram-typings/pull/18 which
caused type generation to be broken

lib/parser.js
@@ -205,7 +205,7 @@ async function requestAndParse(store/*: Store*/) {
 
     let typeClass
 
-    if (['Field', 'Parameters'].includes(type)) {
+    if (!['Field', 'Parameters'].includes(type)) {
       continue // eslint-disable-line no-continue
     }
 